da6d233c9b20caf8f30af4bd702b354c07ded0b2
Unknown <mozartdv@live.ru>
2018-06-01 16:55:28 +1000

    Изменен текст "Продолжить чтение"

--- template-parts/content.php
+++ template-parts/content.php
@@ -36,7 +36,7 @@
 		the_content( sprintf(
 			wp_kses(
 				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'sopki' ),
+				__( 'Продолжить чтение<span class="screen-reader-text"> "%s"</span>', 'sopki' ),
 				array(
 					'span' => array(
 						'class' => array(),
