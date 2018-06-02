8497b7c666cbc9d6472626e6924cfe493e266b9b
mozartdv <mozartdv@live.ru>
2018-06-02 13:46:50 +1000

    Шрифт Материал

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
