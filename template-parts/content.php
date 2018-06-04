f97fb1d214cfd525ab20ce74327d52a805ba9e4d
Unknown <mozartdv@live.ru>
2018-06-04 18:01:00 +1000

    Работаю с кнопкой Рид море

--- template-parts/content.php
+++ template-parts/content.php
@@ -34,18 +34,7 @@
 
 	<div class="entry-content">
 		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Продолжить чтение<span class="screen-reader-text"> "%s"</span>', 'sopki' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
+		the_content('Узнать больше');
 
 		wp_link_pages( array(
 			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sopki' ),
