e7a9ff3b573df4fa4f9cdcd2b63877b2f758be9b
Unknown <mozartdv@live.ru>
2018-06-05 16:08:28 +1000

    Добавил лого в меню

--- header.php
+++ header.php
@@ -31,29 +31,11 @@
 	
 	
 	<header id="masthead" class="site-header">
-	<div class='container center-align'>
-		<div class="site-branding ">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$sopki_description = get_bloginfo( 'description', 'display' );
-			if ( $sopki_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $sopki_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-	</div>	
+	
 
 		<nav>
 			<div class="nav-wrapper white grey-text text-darken-4">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="brand-logo center"><img src="<?php echo get_bloginfo('template_url') ?>/img/logo.png"/></a>
 			<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 			<ul class="menu-font left hide-on-med-and-down">
 			
