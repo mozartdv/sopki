84f1166419d86b5b5a2ddf01dc40464be1509c02
Unknown <mozartdv@live.ru>
2018-06-08 17:36:07 +1000

    Скрипт ГуглАналитики

--- header.php
+++ header.php
@@ -13,6 +13,16 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-44116844-11"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-44116844-11');
+</script>
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
