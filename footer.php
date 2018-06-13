ff156f174577a06ca2bc0698e005833b35f51f15
Unknown <mozartdv@live.ru>
2018-06-13 10:41:04 +1000

    Футер

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 	<footer id="colophon" class="page-footer grey darken-3 site-footer">
 		<div class="container">
 			<div class="row" style="padding-top:25px; padding-bottom:25px">
-			<p class="footer-info">ИА Сопки.online</p>
+			<p class="footer-info">СОПКИ-НА-АМУРЕ</p>
 			<p class="footer-info">Наш e-mail:<a href="mailto:editor@sopkikms.ru">editor@sopkikms.ru</a></p>
 			</div>
 		</div><!-- .site-info -->
